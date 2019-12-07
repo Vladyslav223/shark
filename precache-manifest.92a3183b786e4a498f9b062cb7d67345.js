2618e206efbf7cc6cc49b54e2ad4b515ec9185a1
vladyslav223 <vladyslav223@gmail.com>
2019-12-07 03:08:06 +0200

    Updates

---
+++ precache-manifest.92a3183b786e4a498f9b062cb7d67345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97f5fcb2d3d1fce5cbeedbb79b77ed8",
+    "url": "/shark.git/index.html"
+  },
+  {
+    "revision": "03c9fac39f1bb8fc2846",
+    "url": "/shark.git/static/css/main.7019cbf6.chunk.css"
+  },
+  {
+    "revision": "3729922de782df707899",
+    "url": "/shark.git/static/js/2.26ac2a10.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/shark.git/static/js/2.26ac2a10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03c9fac39f1bb8fc2846",
+    "url": "/shark.git/static/js/main.63648ea9.chunk.js"
+  },
+  {
+    "revision": "5e80e3c2be6a8fef831d",
+    "url": "/shark.git/static/js/runtime-main.687c50a2.js"
+  }
+]);
