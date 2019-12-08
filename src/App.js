ce46c5a48fae56a60ed8ca7ae60c6b9fa900a95d
vladyslav223 <vladyslav223@gmail.com>
2019-12-08 09:01:21 +0200

    almost done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React from 'react';
 
 import {
-  HashRouter, Route, Switch, NavLink,
+  HashRouter, Route, Switch, NavLink, Redirect
 } from 'react-router-dom';
 
 import Contacts from  './components/Contacts/Contacts';
 import HomePage from  './components/HomePage/HomePage';
-import './App.css';
+import Profile from  './components/Profile/Profile';
+import Form from  './components/Form/Form';
 
-const App = () => (
-  <HashRouter>
-    <div className="app">
-      <nav>
-      <NavLink className="nav-link" to="/">Главная</NavLink>
-      <NavLink className="nav-link" exact to="/contacts">Контакты</NavLink>
-      <NavLink className="nav-link" exact to="/profile">Профиль</NavLink>
-      <NavLink className="nav-link" exact to="/autorise">Форма</NavLink>
-      </nav>
-     </div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/contacts" component={Contacts} />
-      </Switch> 
-  </HashRouter>
-);
+import './App.scss';
+
+class App extends React.Component {
+  state = {
+    disableMenu: false,
+  }
+
+  handleLogOut = () => {
+    localStorage.clear();
+    this.setState({
+      redirectToMain: true,
+    })
+  }
+
+  render () { 
+
+    return (
+      <HashRouter>
+          <div className="app">            
+                <nav className="nav">
+                    <NavLink className="nav-link" exact to="/">Главная</NavLink>
+                    <NavLink className="nav-link" exact to="/contacts">Контакты</NavLink>
+                    <NavLink className="nav-link" exact to="/profile">Профиль</NavLink>
+                  </nav>
+                <button
+                  type="button"
+                  className="logout"
+                  onClick={this.handleLogOut}
+                ></button>           
+          </div>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/contacts" component={Contacts} />
+              <Route path="/profile" component={Profile} />
+              <Route render={this.disableMenu} path="/autorise" component={Form} />
+            </Switch> 
+        </HashRouter>
+    );
+  }
+};
 
 export default App;
